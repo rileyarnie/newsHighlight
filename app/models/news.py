a6a39d1ea5488248a06aae7167483bb25ac8675e
Arnold Kibira <arnold.ril3y@gmail.com>
2019-09-09 12:38:45 +0300

    define Articles class and all it's objects

--- app/models/news.py
+++ app/models/news.py
@@ -11,3 +11,17 @@ class Sources:
         self.language = language
         self.country = country
 
+class Articles:
+    """ 
+    Articles class to define Article Objects
+    
+    """
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
+
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage= urlToImage
+        self.publishedAt = publishedAt
